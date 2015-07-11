596b0f06e2b0a61028dd539af44f52821e65fb64
Edward Park <parkes.edward@gmail.com>
2015-07-11 16:33:44 -0400

    added input.val to clear field and fixed spacing issue

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var questionBank ={
-  0: ["Question 1: Do you like cake?", "no"],
-  1: ["Question 2: Do you like pizza?","yes"],
-  2: ["Question 3: Do you like Cookie?","yes"],
-  3: ["Question 4: Do you like Walking?", "no"],
+  0: ["Question 1: Do you like cake?", "1"],
+  1: ["Question 2: Do you like pizza?","beta"],
+  2: ["Question 3: Do you like Cookie?","zeta"],
+  3: ["Question 4: Do you like Walking?", "prime"],
   4: ["Question 5: Do you like running?", "yes"],
   5: ["Question 6: Do you like coding?", "no"],
   6: ["Question 7: Do you like crossfit?", "yes"]
@@ -17,7 +17,7 @@ var i = 0;
 
 function askQuestion(){
   console.log(i);
-
+  $(".inputField").val("");
   if(i >= Object.keys(questionBank).length ){ //note: that program isn't registering this at all.
     console.log("THIS IS THE END!");
     givePlayerScore();
