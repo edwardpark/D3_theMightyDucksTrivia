6807455216d1705654780cd55dc2506fd2e464bf
Edward Park <parkes.edward@gmail.com>
2015-07-10 20:57:39 -0400

    added basic jQuery button binds

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-
 var questionBank ={
   1: ["Question 1: Do you like cake?", false],
   2: ["Question 2: Do you like pizza?",true],
   3: ["Question 3: Do you like Cookie?",true],
   4: ["Question 4: Do you like Walking?", false]
+}
+
+var trueButton = $(".trueB").on("click", function(){
+  console.log("that's true sucka!");
+});
+var falseButton = $(".falseB").on("click",function(){
+  console.log("that's false sucka!");
+});
+
+for(i in questionBank){
+  $("#question").html(questionBank[i][0]);
 }
