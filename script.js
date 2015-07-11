2be9f96667ae10d1ca27f9e59cbc48d010524b59
Edward Park <parkes.edward@gmail.com>
2015-07-11 12:50:57 -0400

    added conditional to checkAnswer

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 var questionBank ={
-  1: ["Question 1: Do you like cake?", false],
+  1: ["Question 1: Do you like cake?", "false"],
   2: ["Question 2: Do you like pizza?",true],
   3: ["Question 3: Do you like Cookie?",true],
   4: ["Question 4: Do you like Walking?", false]
 }
 var i = 1;
 function askQuestion(){
-  $("#question").html("questionBank[1][1]");
+  $("#question").html(questionBank[i][0]);
   getAnswer();
 }
 
+function checkAnswer(){
+  var playerAnswer = $(".inputField").val();
+  var correctAnswer = questionBank[i][1];
+
+  if (playerAnswer == correctAnswer){
+    console.log("great job correct!");
+  }
+
+}
+
+
 function getAnswer(){
   $(".submitButton").on("click",function(){
-
       console.log("event fired!");
-
+      checkAnswer();
   });
   $(".inputField").on("keydown",function(evt){
     if(evt.keyCode == 13){
       console.log("event fired!");
+      //replicate for event fired.
     }
   });
 }
