def49fb766d58260b2dd534debcbe2edcdd0a98e
Edward Park <parkes.edward@gmail.com>
2015-07-10 20:48:43 -0400

    added questionBank object plus seed data

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("hello world!");
+
+var questionBank ={
+  1: ["Question 1: Do you like cake?", false],
+  2: ["Question 2: Do you like pizza?",true],
+  3: ["Question 3: Do you like Cookie?",true],
+  4: ["Question 4: Do you like Walking?", false]
+}
