47bee7923de246392585069a986df548487a79ad
Edward Park <parkes.edward@gmail.com>
2015-07-11 12:36:52 -0400

    added getAnswer listener and confirmed

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ var questionBank ={
   3: ["Question 3: Do you like Cookie?",true],
   4: ["Question 4: Do you like Walking?", false]
 }
+var i = 1;
+function askQuestion(){
+  $("#question").html("questionBank[1][1]");
+  getAnswer();
+}
+
+function getAnswer(){
+  $(".submitButton").on("click",function(){
+
+      console.log("event fired!");
+
+  });
+  $(".inputField").on("keydown",function(evt){
+    if(evt.keyCode == 13){
+      console.log("event fired!");
+    }
+  });
+}
+
+askQuestion();
