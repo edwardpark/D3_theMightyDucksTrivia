053b780d8c5d96efd5d24cb3f14931e33406e0be
Edward Park <parkes.edward@gmail.com>
2015-07-14 10:54:46 -0400

    added general styling to main.css and added surprise file

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+$(window).load(function() {
+
+  $(".endGameView").hide();
+});
+
 var questionBank ={
-  0: ["Question 1: Do you like cake?", "1"],
-  1: ["Question 2: Do you like pizza?","beta"],
-  // 2: ["Question 3: Do you like Cookie?","zeta"],
-  // 3: ["Question 4: Do you like Walking?", "prime"],
-  // 4: ["Question 5: Do you like running?", "yes"],
-  // 5: ["Question 6: Do you like coding?", "no"],
-  // 6: ["Question 7: Do you like crossfit?", "yes"]
+  0: ["Question 1: Do you like cake?", "2"],
+  1: ["Question 2: Do you like pizza?","2"],
+  2: ["Question 3: Do you like Cookie?","2"],
+  3: ["Question 4: Do you like Walking?", "2"],
+  4: ["Question 5: Do you like running?", "2"],
+  5: ["Question 6: Do you like coding?", "2"],
+  6: ["Question 7: Do you like poodles?", "2"]
 }
 
+var questionBank2 = {
+  0:["Magic Mike is loosely inspired by the pre-fame, short-lived stripper career of which of its stars?","beta"]
+}
 
 function givePlayerScore(){
 
@@ -37,12 +45,19 @@ function checkAnswer(){
   var correctAnswer = questionBank[i-1][1];
 
   if (playerAnswer == correctAnswer){
+
+    $(".result1").html("CORRECT!");
+    $(".result1").fadeOut("slow");
+
     //insert border light up and "correct" feedback result
     score++;
 
 
   } else {
-    //insert border light up and "incorrect" feedback result 
+
+    $(".result1").html("INCORRECT!");
+    $(".result1").fadeOut("slow");
+    //insert border light up and "incorrect" feedback result
   }
 
   askQuestion();
@@ -63,9 +78,28 @@ function getAnswer(){
   $(".inputField").on("keydown",function(evt){
     if(evt.keyCode == 13){
 
+            checkAnswer();
+            event.stopPropogation();
       //replicate for event fired.
     }
   });
 }
-$(".endGameView").hide();
 askQuestion();
+
+//MAGIC TIME NSFW FOR INSECURE MEN.
+$(".magictime").on("click",function(){
+  if($("#css").attr("href") == "magic.css"){
+
+
+    $("#css").attr("href","main.css");
+    window.location.reload();
+
+  } else{
+
+    $("#css").attr("href","magic.css");
+    $(".title").html("Trivia");
+    questionBank = questionBank2;
+    askQuestion();
+
+  }
+});
